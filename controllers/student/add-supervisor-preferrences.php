834a1c84a56807e3499076fa75f9ad0a94e1d111
JkOpie <assyaafi96@gmail.com>
2023-02-24 23:44:39 +0800

    ss

--- controllers/student/add-supervisor-preferrences.php
+++ controllers/student/add-supervisor-preferrences.php
@@ -4,30 +4,37 @@
     $conn = setDbConnection();
     session_start();
 
-    $sql = "select * from student_supervisor_preferrences where user_id='".$_SESSION['id']."'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows < 3) {
-
-        $sql = "select * from student_supervisor_preferrences where user_id='".$_SESSION['id']."' and supervisor_id='".$_POST['supervisor_id']."'";
+    if($_POST['supervisor_id'] != "Select Supervisor"){
+        $sql = "select * from student_supervisor_preferrences where user_id='".$_SESSION['id']."'";
         $result = $conn->query($sql);
-
-        if ($result->num_rows == 0) {
-            $sql = "insert into student_supervisor_preferrences (user_id,supervisor_id) values ('".$_SESSION['id']."','".$_POST['supervisor_id']."')";
+    
+        if ($result->num_rows < 3) {
+    
+            $sql = "select * from student_supervisor_preferrences where user_id='".$_SESSION['id']."' and supervisor_id='".$_POST['supervisor_id']."'";
             $result = $conn->query($sql);
-
-            $_SESSION['success'] = 'Supervisor preferences added';
-            header('location: /fyp/supervisor_preferences.php');
+    
+            if ($result->num_rows == 0) {
+                $sql = "insert into student_supervisor_preferrences (user_id,supervisor_id) values ('".$_SESSION['id']."','".$_POST['supervisor_id']."')";
+                $result = $conn->query($sql);
+    
+                $_SESSION['success'] = 'Supervisor preferences added';
+                header('location: /fyp/supervisor_preferences.php');
+            }else{
+                $_SESSION['error'] = 'Error : Preference Supervisor already exists!';
+                header('location: /fyp/supervisor_preferences.php');
+            }
+    
+          
         }else{
-            $_SESSION['error'] = 'Error : Preference Supervisor already exists!';
+            $_SESSION['error'] = 'Maximum 3 supervisor preferences!';
             header('location: /fyp/supervisor_preferences.php');
         }
-
-      
     }else{
         $_SESSION['error'] = 'Maximum 3 supervisor preferences!';
         header('location: /fyp/supervisor_preferences.php');
     }
+
+    
 
 
 ?>
